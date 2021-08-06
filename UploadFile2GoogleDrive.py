e9e4fcfc3be9c5e360d4e827a00d5e774399b343
Sh470nGR <88251895+Sh470nGR@users.noreply.github.com>
2021-08-06 06:50:40 +0300

    Update UploadFile2GoogleDrive.py

--- UploadFile2GoogleDrive.py
+++ UploadFile2GoogleDrive.py
@@ -1,20 +1,17 @@
 # download the chrome driver from here
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyautogui
 import glob
 
-# The PATH to the chromedriver you downloaded
-PATH = "C:\\Program Files (x86)\\chromedriver.exe"
+PATH = "path to the driver file" + "\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-# The location of the folder in goolge drive 
-driver.get("https://drive.google.com/drive/folders/xxxxxxxxxxxxxxxxxxxxxxx")
-
-list_of_files = glob.glob("folder path location on the os")
+driver.get("driver folder URL path")
 
 search = driver.find_element_by_name("identifier")
 search.send_keys("your email address")
@@ -26,9 +23,28 @@ time.sleep(3)
 search.send_keys(Keys.RETURN)
 time.sleep(5)
 
+list_of_files = glob.glob("your directory path" + "\\*")
+list1 = []
 for file in list_of_files:
-    webdriver.ActionChains(driver).key_down(Keys.SHIFT).send_keys("u").perform()
-    time.sleep(2)
-    pyautogui.write(file)
-    time.sleep(1)
-    pyautogui.press('enter')
+    list1.append(file + "\n")
+
+#create a txt file for the program and it will check if the files is uploaded allready or not 
+f = open('the path of the txt file', "r+")
+list2 = []
+for line in f:
+    list2.append(line)
+list_of_unuploaded_files = [x for x in list1 if not x in list2]
+
+if len(list_of_unuploaded_files) == 0:
+    print("no new files to upload")
+else:
+    for file in list_of_unuploaded_files:
+        webdriver.ActionChains(driver).key_down(Keys.SHIFT).send_keys("u").perform()
+        time.sleep(2)
+        pyautogui.write(file)
+        time.sleep(1)
+        pyautogui.press('enter')
+        f.write(file)
+
+f.close()
+driver.close()
